f5caf6d5f17813b785420ca80c4c59a4bd19c1ab
inbarajan <rajaninba6@gmail.com>
2023-10-12 11:42:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import "./App.css";
 import bg from "./assets/images/tennis-ball.jpg";
-import BG from "./assets/images/bg.png";
+// import BG from "./assets/images/bg.png";
 import csk from "./assets/images/csk.jpg";
 import tennis from "./assets/images/tennis.jpg";
 import top from "./assets/images/tennis-tennis-ball-svgrepo-com.svg";
@@ -19,9 +20,16 @@ import linkedin from "./assets/svg/linkedin.svg";
 import whatapp from "./assets/svg/whatapp.svg";
 import youtube from "./assets/svg/youtube.svg";
 
+// import mail from "./assets/svg/envelope-svgrepo-com.svg";
+// import Geo from "./assets/svg/location-marker-svgrepo-com.svg";
+// import phone from "./assets/svg/phone-svgrepo-com.svg";
+
 function App() {
+  const location =
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7859.290973389918!2d78.15657812776354!3d9.963424603111491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b00c717eb945659%3A0x1e0d5bc0fcb99d32!2sBlue%20Ocean%20Sports%20Arena%20(BOSA)!5e0!3m2!1sen!2sin!4v1697008036770!5m2!1sen!2sin";
   const [visable, setVisable] = useState(false);
 
+  // ball visiable function
   const toggleVisible = () => {
     const scrolled = document.documentElement.scrollTop;
 
@@ -32,6 +40,7 @@ function App() {
     }
   };
 
+  // scroll Top function
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -39,14 +48,9 @@ function App() {
     });
   };
 
-  // console.log(new Date());
-  // var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
-  // var distance = countDownDate - new Date();
-
-  // console.log(Math.floor(distance / (1000 * 60 * 60 * 24)));
-
   window.addEventListener("scroll", toggleVisible);
 
+  // responsive nav tab
   const [showNav, setShowNav] = useState(0);
 
   const HandelTab = () => {
@@ -69,11 +73,9 @@ function App() {
     }
   }
 
-  // Day ,hours, mins and secs
-
   // current Date
   var currentDate = new Date("Jan 5, 2024 15:37:25").getTime();
-
+  // Day ,hours, mins and secs
   setInterval(function () {
     var countDown = currentDate - new Date().getTime();
 
@@ -89,50 +91,63 @@ function App() {
     document.getElementById("demo").innerHTML =
       day + "D " + hours + "h " + mins + "m " + secs + "s";
   }, 1000);
+
+  // scroll animation
+  window.addEventListener(
+    "scroll",
+    () => {
+      document.body.style.setProperty(
+        "--scroll",
+        window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+      );
+    },
+    false
+  );
+
   return (
     <>
       {/* header */}
       <header id="home">
+        {/* <div className="position-relative"> */}
         <div className="nav" id="nav">
           <div className="logo">LOGO</div>
-          <div className="position-relative">
-            <div className="drawer">
-              {showNav === 0 ? (
-                <button className="btn" onClick={HandelTab}>
-                  <i className="bi bi-list"></i>
-                </button>
-              ) : (
-                <button className="btn" onClick={(e) => HandelClose()}>
-                  <i className="bi bi-x-lg"></i>
-                </button>
-              )}
-            </div>
-            <div className="nav-item  active">
-              <ul>
-                <li>
-                  <a href="#home">HOME</a>
-                  {/* <div className="line"></div> */}
-                </li>
-                <li>
-                  <a href="#matches">MATCHES</a>
-                  {/* <div className="line"></div> */}
-                </li>
-                <li>
-                  <a href="#player">PLAYERS</a>
-                  {/* <div className="line"></div> */}
-                </li>
-                <li>
-                  <a href="#blog">BLOG</a>
-                  {/* <div className="line"></div> */}
-                </li>
-                <li>
-                  <a href="#contact">CONTACT</a>
-                  {/* <div className="line"></div> */}
-                </li>
-              </ul>
-            </div>
+          <div className="drawer">
+            {showNav === 0 ? (
+              <button className="btn" onClick={HandelTab}>
+                <i className="bi bi-list"></i>
+              </button>
+            ) : (
+              <button className="btn" onClick={(e) => HandelClose()}>
+                <i className="bi bi-x-lg"></i>
+              </button>
+            )}
+          </div>
+          <div className="nav-item  active">
+            <ul>
+              <li>
+                <a href="#home">HOME</a>
+                {/* <div className="line"></div> */}
+              </li>
+              <li>
+                <a href="#matches">MATCHES</a>
+                {/* <div className="line"></div> */}
+              </li>
+              <li>
+                <a href="#player">PLAYERS</a>
+                {/* <div className="line"></div> */}
+              </li>
+              <li>
+                <a href="#blog">BLOG</a>
+                {/* <div className="line"></div> */}
+              </li>
+              <li>
+                <a href="#contact">CONTACT</a>
+                {/* <div className="line"></div> */}
+              </li>
+            </ul>
           </div>
         </div>
+        {/* </div> */}
 
         {/* SideNav */}
         <div className="Side-nav" id="Side-nav">
@@ -156,7 +171,6 @@ function App() {
             </ul>
           </div>
         </div>
-
         {/*Header Content  */}
         <div className="img">
           <img src={bg} alt="bg" />
@@ -187,6 +201,7 @@ function App() {
           </div>
         </div>
       </header>
+
       {/* Player section */}
       <div className="container">
         <div className="matches" id="matches">
@@ -551,7 +566,7 @@ function App() {
               </ol>
             </aside>
           </section> */}
-          <div class="carousel-wrapper" id="carousel">
+          <div className="carousel-wrapper" id="carousel">
             <video width="443" autoPlay loop muted>
               <source src={tennisVid_2} type="video/mp4" />
             </video>
@@ -566,15 +581,18 @@ function App() {
             </video>
           </div>
         </div>
+
         <div className="position-relative">
           <div className="position-absolute end-0 ">
             <button className="btn" type="button" onClick={() => tap("<")}>
-              {">"}
+              {/* {">"} */}
+              <i className="bi bi-arrow-right-short"></i>
             </button>
           </div>
           <div className="position-absolute start-0">
             <button className="btn" type="button" onClick={() => tap(">")}>
-              {"<"}
+              {/* {"<"} */}
+              <i className="bi bi-arrow-left-short"></i>
             </button>
           </div>
         </div>
@@ -667,52 +685,87 @@ function App() {
       </div>
 
       {/* Contact */}
-      <div className="contact">
-        <div className="mt-4 mb-4 m-5 title fs-3" id="contact">
-          Contact
-        </div>
-        <div className="d-flex align-item-center justify-content-center gap-5">
-          <div className="col-6 mt-4 mb-5">
-            <form noValidate>
-              <div className="form-floating mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="User Name"
-                />
-                <label htmlFor="floatingPassword">
-                  <i className="bi bi-person fs-5"></i>User Name
-                </label>
-              </div>
-              <div className="form-floating mb-3">
-                <input
-                  type="email"
-                  className="form-control"
-                  id="floatingInput"
-                  placeholder="name@example.com"
-                />
-                <label htmlFor="floatingInput">
-                  <i className="bi bi-envelope fs-5"></i>Email address
-                </label>
-              </div>
-              <div className="form-floating">
-                <textarea
-                  className="form-control"
-                  placeholder="Leave a comment here"
-                  id="floatingTextarea2"
-                  style={{ height: "150px" }}
-                ></textarea>
-                <label htmlFor="floatingTextarea2">
-                  <i className="bi bi-chat-dots fs-5"></i>
-                  Comments
-                </label>
-              </div>
-              <div className="text-center mb-3 mt-3">
-                <button className="btn btn-md" type="submit">
-                  Send
-                </button>
-              </div>
-            </form>
+
+      {/* <div className="contact-details d-flex justify-content-center text-center">
+          <div className="contact-item">
+          <i className="bi bi-telephone title fs-4"></i> :
+            <a href="tel:+91 6379219816">+91 6379219816</a>
+            </div>
+            <div className="contact-item">
+            <i className="bi bi-envelope title fs-4"></i> :
+            <a href="mailto:winioa@gmail.com">winioa@gmail.com</a>
+            <div className="location">
+              <i className="bi bi-geo-alt-fill title fs-4"></i>
+              <a
+                href="https://maps.app.goo.gl/cvbh8ji2EDrh35zeA"
+                target="blank"
+                >
+                Location
+                </a>
+                </div>
+                </div>
+        </div> */}
+
+      <div className="contact" id="contact">
+        <div className="mt-4 mb-4 p-3 title fs-3">Contact</div>
+        {/* <div className="row"> */}
+        <div className="d-flex justify-content-center gap-4 contact-form">
+          <div className="col-6 mt-4">
+            <div className="contact-form">
+              <form noValidate>
+                <div className="form-floating mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="User Name"
+                  />
+                  <label htmlFor="floatingPassword">
+                    <i className="bi bi-person fs-5"></i>User Name
+                  </label>
+                </div>
+                <div className="form-floating mb-3">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="floatingInput"
+                    placeholder="name@example.com"
+                  />
+                  <label htmlFor="floatingInput">
+                    <i className="bi bi-envelope fs-5"></i> Email address
+                  </label>
+                </div>
+                <div className="form-floating">
+                  <textarea
+                    className="form-control"
+                    placeholder="Leave a comment here"
+                    id="floatingTextarea2"
+                    style={{ height: "150px" }}
+                  ></textarea>
+                  <label htmlFor="floatingTextarea2">
+                    <i className="bi bi-chat-dots fs-5"></i> Comments
+                  </label>
+                </div>
+                <div className="d-flex gap-3 text-start mb-3 mt-3">
+                  <button className="btn btn-md" type="submit">
+                    Send
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          {/* Adress */}
+          <div className="mt-4 mb-5">
+            <div className="contact-map">
+              <iframe
+                src={location}
+                width="550"
+                height="300"
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              />
+            </div>
+            {/* </div> */}
           </div>
         </div>
       </div>
@@ -722,20 +775,51 @@ function App() {
         <div className="text-center bg-white text-dark p-4 h-120">
           <div className="address">
             <div className="footer-item">
-              <div className="d-flex justify-content-between align-item-center">
-                {/* Adress */}
-                <div className="footer-item_1 w-50 text-start p-3 ">
+              <div className="d-flex justify-content-around">
+                {/* About */}
+                <div className="footer-item_1 w-50 text-start p-2">
+                  <div className="fs-2 text fw-bolder">
+                    <i className="bi bi-book-half"></i> About As
+                  </div>
+                  <p className="fs-5 text-left">
+                    Welcome to BOSA sports club! We are a passionate community
+                    of sports enthusiasts dedicated to promoting physical
+                    fitness, camaraderie, and the love of sports.
+                  </p>
+                </div>
+                {/* Social NetWorks */}
+                <div className="footer-item_2 w-50 text-start p-2 fw-bolder">
+                  <div className="fs-2 text">
+                    <i className="bi bi-globe2"></i> Find Us
+                  </div>
+                  <div className="d-flex gap-4 fs-2 rotate">
+                    <a href="#instagram" target="blank">
+                      <img src={face} alt="img" height={40} />
+                    </a>
+                    <a href="#instagram" target="blank">
+                      <img src={linkedin} alt="linkedin" height={45} />
+                    </a>
+                    <a href="#instagram" target="blank">
+                      <img src={insta} height={45} alt="insta" />
+                    </a>
+                    <a href="#instagram" target="blank">
+                      <img src={youtube} alt="img" height={45} />
+                    </a>
+                  </div>
+                </div>
+
+                <div className="footer-item_3 w-50 text-start p-2">
                   <div className="fs-3 text fw-bolder">
                     <i className="bi bi-geo-alt-fill"></i>Address
                   </div>
                   <a
-                    className=""
                     href="https://maps.app.goo.gl/WP8sLYCeveQRZwD29"
                     target="blank"
                   >
                     14, Alagar Kovil Main Rd, near Inthiya Super Market, Surya
                     Nagar, Madurai, Tamil Nadu 625007
                   </a>
+
                   <div className="mt-2">
                     <i className="bi bi-telephone text fs-4"></i> :
                     <a href="tel:+91 6379219816">+91 6379219816</a>
@@ -745,62 +829,39 @@ function App() {
                     <a href="mailto:winioa@gmail.com">winioa@gmail.com</a>
                   </div>
                 </div>
-                {/* Social NetWorks */}
-                <div className="footer-item_2 w-50 text-start p-3 fw-bolder">
-                  <div className="fs-2 text">
-                    <i className="bi bi-globe2"></i> Social Media
-                  </div>
-                  <div className="d-flex gap-3 fs-2 m-1">
-                    <a href="#instagram" target="blank">
-                      <img src={insta} height={35} alt="insta" />
-                    </a>
-                    <a href="#instagram" target="blank">
-                      <img src={face} alt="img" height={30} />
-                    </a>
-                    <a href="#instagram" target="blank">
-                      <img src={linkedin} alt="linkedin" height={40} />
-                    </a>
-                    <a href="#instagram" target="blank">
-                      <img src={youtube} alt="img" height={40} />
-                    </a>
-                  </div>
-                </div>
-
-                {/* About */}
-                <div className="footer-item_3 w-50 text-start p-3">
-                  <div className="fs-2 text fw-bolder">
-                    <i className="bi bi-book-half"></i> About As
-                  </div>
-                  <p className="fs-5 text-left">
-                    Welcome to BOSA sports club! We are a passionate community
-                    of sports enthusiasts dedicated to promoting physical
-                    fitness, camaraderie, and the love of sports. Founded with
-                    the vision of fostering a healthy and active lifestyle, our
-                    club offers a wide range of sporting activities, from tennis
-                    and soccer to swimming and cycling.
-                  </p>
-                </div>
               </div>
             </div>
-            <div className="text-dark fs-6 mt-3">
-              Created By :
-              <span>
-                <a href="https://wizinoa.com/" target="blank" className="text">
-                  WizInoa💡
-                </a>
-              </span>
-            </div>
+          </div>
+          <div className="text-dark fs-6 mt-3">
+            Created By :
+            <span>
+              <a href="https://wizinoa.com/" target="blank" className="text">
+                WizInoa💡
+              </a>
+            </span>
           </div>
         </div>
       </div>
+
       {/* whatsApp */}
       <div className="position-relative">
-        <div className="position-fixed position-absolute bottom-0 p-4 mb-4 mt-4 start-0">
+        <div className="position-fixed position-absolute bottom-0 start-0 rotate">
           <a href="https://wa.me/6381114224">
             <img src={whatapp} height={40} alt="whatsapp" />
           </a>
         </div>
       </div>
+      {/* <div className="position-relative mb-3">
+        <div className="position-fixed position-absolute top-50 start-0 ">
+          <a href="tel:+91 6381114224">
+            <img src={phone} height={40} alt="phone" />
+          </a>
+
+          <a href="tel:+91 6381114224">
+            <img src={Geo} height={40} alt="phone" />
+          </a>
+        </div>
+      </div> */}
 
       {/* top button */}
       <div className="position-relative">
@@ -813,8 +874,8 @@ function App() {
               src={top}
               width={40}
               height={50}
-              alt="top"
               className="animation"
+              alt="top"
             />
           </Link>
         </div>
